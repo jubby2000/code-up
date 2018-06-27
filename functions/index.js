f7a1d119ede11804ffebb5816e7fd7dac5455e3b
Farah Quader <fyquader@gmail.com>
2018-06-26 20:32:26 -0700

    Added firebase database queries

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+// Import Firebase-admin and initialize app.
+const admin = require('firebase-admin');
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+const config = {
+  apiKey: "AIzaSyD7t2UELyolG2mRLVk1XvYoJi30TffOo8A",
+  authDomain: "code-up-927cb.firebaseapp.com",
+  databaseURL: "https://code-up-927cb.firebaseio.com/"
+};
+
+admin.initializeApp(config);
+
+// Reference database.
+const rootRef = admin.database().ref();
+// const question_set = rootRef.child('Questions')
+//                             .orderByChild('Tags')
+//                             .limitToFirst(1).on('value', snap => {
+//                               console.log('value', snap.val());
+//                             });
 // Import the Dialogflow module from the Actions on Google client library.
 const {
   dialogflow,
@@ -12,18 +32,19 @@ const functions = require('firebase-functions');
 
 // Instantiate the Dialogflow client.
 const app = dialogflow({ debug: true });
-
+console.log("line 35 =============================");
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
 app.intent('Default Welcome Intent', (conv) => {
+  console.log("line 38 =======================================");
   conv.ask(new Permission({
     context: 'Hi there, to get to know you better',
     permissions: 'NAME',
   }));
 });
-
 // Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
 // agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
 app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
+  console.log("line 47 ==================================");
   if (!permissionGranted) {
     conv.ask(`Ok, no worries. What programming language would you like to learn?`);
   } else {
@@ -59,6 +80,17 @@ app.intent('programming language', (conv, { programmingLanguage }) => {
 //     display: 'WHITE',
 //   })
 // };
+
+app.intent('programming language - select.number', (conv, { language, number }) => {
+  let question_set;
+  rootRef.child('Questions').orderByChild('Tags').limitToFirst(1).on('value', snap => {
+    question_set = snap.val().Question;
+  });
+  conv.ask(
+    `Got it, ${language} and ${number} questions. Let's do it.` +
+    `${question_set}`
+  );
+});
 
 app.intent('programming language - select.number', (conv, { language, number }) => {
   conv.close(`Got it, ${language} and ${number} questions. Let's do it.`);
