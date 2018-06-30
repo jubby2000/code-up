5d4282ffc3a8fab101f6e28aeec43403aa6c646f
Farah Quader <fyquader@gmail.com>
2018-06-29 22:53:57 -0700

    Change query to filter database and dynamically choose questions

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,12 @@
 
 // Import Firebase-admin and initialize app.
 const admin = require('firebase-admin');
-
+// const { API_KEY } = require('../keys.js');
+// const keys = require('../keys');
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
 const config = {
-  apiKey: "AIzaSyD7t2UELyolG2mRLVk1XvYoJi30TffOo8A",
+  apiKey: 'AIzaSyD7t2UELyolG2mRLVk1XvYoJi30TffOo8A',
   authDomain: "code-up-927cb.firebaseapp.com",
   databaseURL: "https://code-up-927cb.firebaseio.com/"
 };
@@ -17,12 +18,12 @@ admin.initializeApp(config);
 const rootRef = admin.database().ref();
 
 // Global reference for current questions
-let question_set;
+// let question_set;
 
 const successSound = 'https://actions.google.com/sounds/v1/crowds/battle_crowd_celebrate_stutter.ogg';
 const failSound = 'https://actions.google.com/sounds/v1/cartoon/cartoon_boing.ogg';
 const calculateSound = 'https://actions.google.com/sounds/v1/office/keyboard_typing_fast.ogg';
-const gameOverSound = 'https://actions.google.com/sounds/v1/cartoon/cartoon_metal_thunk.ogg'; 
+const gameOverSound = 'https://actions.google.com/sounds/v1/cartoon/cartoon_metal_thunk.ogg';
 
 const {
   dialogflow,
@@ -38,7 +39,7 @@ const app = dialogflow({ debug: true });
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
 app.intent('Default Welcome Intent', (conv) => {
   conv.data.count = 0;
-  let score = conv;
+  // let score = conv;
   conv.ask(new Permission({
     context: 'Hi there, to get to know you better',
     permissions: 'NAME',
@@ -55,14 +56,17 @@ app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
     conv.data.userName = conv.user.name.given;
     conv.ask(`Thanks, ${conv.data.userName}. What programming language would you like to learn?`);
   }
-});
+}); 
 
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
 app.intent('programming language', (conv, { programmingLanguage }) => {
   conv.data.count = 0;
+  const getQuestions = rootRef.child('questions').orderByChild('tags').equalTo(programmingLanguage).limitToFirst(5).once('value');
   return getQuestions.then(snapshot => {
-    question_set = snapshot.val();
+    let question_set = Object.keys(snapshot.val()).map( function(key) {
+      return snapshot.val()[key]
+    });
     conv.data.questions = question_set;
     conv.data.answer = question_set[0].correctAnswer;
     let answers = question_set[0].wrongAnswer.concat([conv.data.answer]);
@@ -71,7 +75,6 @@ app.intent('programming language', (conv, { programmingLanguage }) => {
                     `a. ${answers[0]}?\n` +
                     `b. ${answers[1]}? or\n` +
                     `c. ${answers[2]}?`);
-    // return conv.ask();
   })
 });
 
@@ -83,8 +86,8 @@ app.intent('programming language - answer1', (conv, { answer }) => {
   } else {
     conv.ask(`<speak><audio src="${failSound}"></audio>Sorry, the answer was ${conv.data.answer}. Next question: ${conv.data.questions[1].question}</speak>`);
   }
-  conv.data.answer = question_set[1].correctAnswer;
-  let answers = question_set[1].wrongAnswer.concat([conv.data.answer]);
+  conv.data.answer = conv.data.questions[1].correctAnswer;
+  let answers = conv.data.questions[1].wrongAnswer.concat([conv.data.answer]);
   conv.ask(`Is it\n` +
     `a. ${answers[0]}?\n` +
     `b. ${answers[1]}? or\n` +
@@ -99,8 +102,8 @@ app.intent('programming language - answer2', (conv, { answer }) => {
   } else {
     conv.ask(`<speak><audio src="${failSound}"></audio>Nope, the answer was ${conv.data.answer}. Here's your next one: ${conv.data.questions[2].question}</speak>`);
   }
-  conv.data.answer = question_set[2].correctAnswer;
-  let answers = question_set[2].wrongAnswer.concat([conv.data.answer]);
+  conv.data.answer = conv.data.questions[2].correctAnswer;
+  let answers = conv.data.questions[2].wrongAnswer.concat([conv.data.answer]);
   conv.ask(`Is it\n` +
     `a. ${answers[0]}?\n` +
     `b. ${answers[1]}? or\n` +
@@ -114,8 +117,8 @@ app.intent('programming language - answer3', (conv, { answer }) => {
   } else {
     conv.ask(`<speak><audio src="${failSound}"></audio>Nope, the answer was ${conv.data.answer}. Here's your next one: ${conv.data.questions[3].question}</speak>`);
   }
-  conv.data.answer = question_set[3].correctAnswer;
-  let answers = question_set[3].wrongAnswer.concat([conv.data.answer]);
+  conv.data.answer = conv.data.questions[3].correctAnswer;
+  let answers = conv.data.questions[3].wrongAnswer.concat([conv.data.answer]);
   conv.ask(`Is it\n` +
     `a. ${answers[0]}?\n` +
     `b. ${answers[1]}? or\n` +
@@ -129,8 +132,8 @@ app.intent('programming language - answer4', (conv, { answer }) => {
   } else {
     conv.ask(`<speak><audio src="${failSound}"></audio>Nope, the answer was ${conv.data.answer}. Here's your next one: ${conv.data.questions[4].question}</speak>`);
   }
-  conv.data.answer = question_set[4].correctAnswer;
-  let answers = question_set[4].wrongAnswer.concat([conv.data.answer]);
+  conv.data.answer = conv.data.questions[4].correctAnswer;
+  let answers = conv.data.questions[4].wrongAnswer.concat([conv.data.answer]);
   conv.ask(`Is it\n` +
     `a. ${answers[0]}?\n` +
     `b. ${answers[1]}? or\n` +
@@ -155,8 +158,8 @@ app.intent('programming language - answer5', (conv, { answer }) => {
 
 // app.intent('programming language - select.number', (conv, { language, number }) => {
 //   return getQuestions.then(snapshot => {
-//     let question_set = snapshot.val();
-//     conv.data.questions = question_set;
+//     let conv.data.questions = snapshot.val();
+//     conv.data.questions = conv.data.questions;
 //     return conv.close(
 //       `Got it, ${language} and ${number} questions. Let's do it. ` +
 //       `${question_set[0].Question}`
@@ -187,7 +190,7 @@ app.intent('programming language - answer5', (conv, { answer }) => {
 
 
 
-const getQuestions = rootRef.child('questions').orderByChild('tags').limitToFirst(5).once('value');
+// const getQuestions = rootRef.child('questions').orderByChild('tags').equalTo('Ruby').limitToFirst(5).once('value');
 // app.intent('programming language - select.number', (conv, { language, number }) => {
 //   conv.close(`Got it, ${language} and ${number} questions. Let's do it.`);
 // });
